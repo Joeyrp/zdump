97dcc95347e95b0d08ecf9b7b92caaea4f8027c2
Joey Pollack <joey@joeyrp.com>
2025-01-26 01:42:26 -0500

    Basic hex dump working

--- src/main.zig
+++ src/main.zig
@@ -34,23 +34,47 @@ pub fn main() !u8 {
         return 1;
     }
 
-    const cwd = try std.fs.selfExeDirPathAlloc(allocator);
-    defer allocator.free(cwd);
-    const opt_cwd_path = std.fs.path.dirname(cwd);
+    if (std.mem.eql(u8, args[1], "--dump-test")) {
+        const file = try std.fs.cwd().createFile("test_file", .{ .read = true });
+        defer file.close();
 
-    if (opt_cwd_path) |real_path| {
-        std.debug.print("program dir: {s}\n", .{real_path});
+        _ = try file.write(&[_]u8{ 0xAB, 1, 2, 4, 8, 16, 32, 127, 128 });
+
+        std.debug.print("test file dumped\n", .{});
+        return 0;
     }
 
-    const cwd_path = try std.fs.cwd().realpathAlloc(allocator, ".");
-    defer allocator.free(cwd_path);
-    std.debug.print("working dir: {s}\n", .{cwd_path});
+    // const cwd = try std.fs.selfExeDirPathAlloc(allocator);
+    // defer allocator.free(cwd);
+    // const opt_cwd_path = std.fs.path.dirname(cwd);
+
+    // if (opt_cwd_path) |real_path| {
+    //     std.debug.print("program dir: {s}\n", .{real_path});
+    // }
+
+    // const cwd_path = try std.fs.cwd().realpathAlloc(allocator, ".");
+    // defer allocator.free(cwd_path);
+    // std.debug.print("working dir: {s}\n", .{cwd_path});
+
+    std.debug.print("dumping file: {s}\n", .{args[1]});
 
     // WORKS!
-    std.debug.print("TEST PRINT: {x:02} {x:02} {x:02} {x:02} \n", .{ 5, 16, 32, 128 });
+    // std.debug.print("TEST PRINT: {x:02} {x:02} {x:02} {x:02} \n", .{ 5, 16, 32, 128 });
 
-    // const file = try std.fs.cwd().openFile(args[1], .{});
-    // defer file.close();
+    const file = try std.fs.cwd().openFile(args[1], .{});
+    defer file.close();
+
+    const stat = try file.stat();
+    const buffer = try allocator.alloc(u8, stat.size);
+    defer allocator.free(buffer);
+    const bytes_read = try file.readAll(buffer);
+    _ = bytes_read;
+
+    for (buffer) |byte| {
+        std.debug.print("{X:02} ", .{byte});
+    }
+
+    std.debug.print("\n", .{});
 
     return 0;
 }
